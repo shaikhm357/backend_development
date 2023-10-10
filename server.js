ba9a502bd9d739b89906437eafd912e0642d4290
Shaikh Mahboob <mahboob.shaikh@finclusionsolutions.com>
2023-10-11 00:08:56 +0530

    send json response

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const http = require('http')
 
+const todos = [
+    { id: 1, text: "Todo One" },
+    { id: 2, text: "Todo Two" },
+    { id: 3, text: "Todo Three" },
+]
+
 const server = http.createServer((req, res) => {
-    res.setHeader('Content-Type', 'text/html')
+    res.setHeader('Content-Type', 'application/json')
     res.setHeader('X-Powered-By', 'Node.js')
-    res.write("<h1>Hello<h1>")
-    res.write("<h1>Hello world<h1>")
-    res.end()
+    res.end(JSON.stringify({
+        success: true,
+        data: todos
+    }))
 })
 
 const PORT = 5000
