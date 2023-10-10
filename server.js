99db210c30b56ace25e6dece697c9956cc1c0333
Shaikh Mahboob <mahboob.shaikh@finclusionsolutions.com>
2023-10-11 00:58:11 +0530

    added get req in node

--- server.js
+++ server.js
@@ -7,27 +7,38 @@ const todos = [
 ]
 
 const server = http.createServer((req, res) => {
-
-    console.log(req.body)
+    const { method, url } = req
 
     let body = []
+
     req
         .on('data', (chunk) => {
             body.push(chunk)
         })
         .on('end', () => {
             body = Buffer.concat(body).toString()
-            console.log(body)
+
+            let status = 404
+
+            const response = {
+                success: false,
+                data: null
+            }
+
+            if (method === 'GET' && url === '/todos') {
+                status = 200
+                response.success = true
+                response.data = todos
+            }
+
+            res.writeHead(status, {
+                'Content-Type': 'application/json',
+                'X-Powered-By': 'Node.js'
+            })
+
+            res.end(JSON.stringify(response))
         })
 
-    res.writeHead(404, {
-        'Content-Type': 'application/json',
-        'X-Powered-By': 'Node.js'
-    })
-    res.end(JSON.stringify({
-        success: true,
-        data: todos
-    }))
 })
 
 const PORT = 5000
