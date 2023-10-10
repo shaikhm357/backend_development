036b79391f91e81183d36903822431c9b5eeed9b
Shaikh Mahboob <mahboob.shaikh@finclusionsolutions.com>
2023-10-11 00:18:52 +0530

    learned status codes

--- server.js
+++ server.js
@@ -7,11 +7,15 @@ const todos = [
 ]
 
 const server = http.createServer((req, res) => {
-    res.setHeader('Content-Type', 'application/json')
-    res.setHeader('X-Powered-By', 'Node.js')
+
+    res.writeHead(404, {
+        'Content-Type': 'application/json',
+        'X-Powered-By': 'Node.js'
+    })
     res.end(JSON.stringify({
-        success: true,
-        data: todos
+        success: false,
+        error: "Not Found",
+        data: null
     }))
 })
 
