8ed1e5b7cddce52664d2efddf871c87f9c7180a8
Shaikh Mahboob <mahboob.shaikh@finclusionsolutions.com>
2023-10-14 23:58:13 +0530

    initail express setup install express and dotenv and configured same

--- server.js
+++ server.js
@@ -1,57 +1,10 @@
-const http = require('http')
+const express = require('express')
+const dotenv = require('dotenv')
 
-const todos = [
-    { id: 1, text: "Todo One" },
-    { id: 2, text: "Todo Two" },
-    { id: 3, text: "Todo Three" },
-]
+dotenv.config({ path: './config/config.env' })
 
-const server = http.createServer((req, res) => {
-    const { method, url } = req
+const app = express()
 
-    let body = []
+const PORT = process.env.PORT || 8000
 
-    req
-        .on('data', (chunk) => {
-            body.push(chunk)
-        })
-        .on('end', () => {
-            body = Buffer.concat(body).toString()
-
-            let status = 404
-
-            const response = {
-                success: false,
-                data: null
-            }
-
-            if (method === 'GET' && url === '/todos') {
-                status = 200
-                response.success = true
-                response.data = todos
-            } else if (method === 'POST' && url === '/todos') {
-                const { id, text } = JSON.parse(body)
-                if (!id || !text) {
-                    status = 400
-                } else {
-                    todos.push({ id, text })
-                    status = 201
-                    response.success = true
-                    response.data = todos
-                }
-
-            }
-
-            res.writeHead(status, {
-                'Content-Type': 'application/json',
-                'X-Powered-By': 'Node.js'
-            })
-
-            res.end(JSON.stringify(response))
-        })
-
-})
-
-const PORT = 5000
-
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
