622c4b8189084fcfd898a707722e90f426eaa806
Shaikh Mahboob <mahboob.shaikh@finclusionsolutions.com>
2023-10-15 00:25:09 +0530

    added CRUD routes

--- server.js
+++ server.js
@@ -5,6 +5,26 @@ dotenv.config({ path: './config/config.env' })
 
 const app = express()
 
+app.get('/api/v1/bootcamp', (req, res) => {
+    res.status(200).json({ success: true, msg: "Show All Bootcamp" })
+})
+
+app.get('/api/v1/bootcamp/:id', (req, res) => {
+    res.status(200).json({ success: true, msg: `Show Bootcamp ${req.params.id}` })
+})
+
+app.post('/api/v1/bootcamp', (req, res) => {
+    res.status(200).json({ success: true, msg: "Create New Bootcamp" })
+})
+
+app.put('/api/v1/bootcamp/:id', (req, res) => {
+    res.status(200).json({ success: true, msg: `Update Bootcamp ${req.params.id}` })
+})
+
+app.delete('/api/v1/bootcamp/:id', (req, res) => {
+    res.status(200).json({ success: true, msg: `Delete Bootcamp ${req.params.id}` })
+})
+
 const PORT = process.env.PORT || 8000
 
 app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
