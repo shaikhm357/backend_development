7a5ac478d8a8cb9b37fe2c65d261cddfea264faa
Shaikh Mahboob <mahboob.shaikh@finclusionsolutions.com>
2023-10-16 12:12:23 +0530

    created Bootcamp model and connected mongoDB

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
+const colors = require('colors')
 const connectDB = require('./config/db')
 
 dotenv.config({ path: './config/config.env' })
@@ -24,11 +25,11 @@ app.use('/api/v1/bootcamp', bootcamp)
 
 const PORT = process.env.PORT || 8000
 
-const server = app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+const server = app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
 
 // handle unhandled promise rejections
 process.on('unhandledRejection', (err, promise) => {
-    console.log(`Error: ${err.message}`)
+    console.log(`Error: ${err.message}`.red)
     // close server and exit process 
     server.close(() => process.exit(1))
 })
