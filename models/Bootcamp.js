b3ea3fd9bff78cc197cd5c0ec2e628086451cf3c
Shaikh Mahboob <mahboob.shaikh@finclusionsolutions.com>
2023-10-18 00:37:35 +0530

    learn mongoose's pre middleware and created slug

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const slugify = require('slugify')
 
 const BootcampSchema = new mongoose.Schema(
     {
@@ -108,5 +109,10 @@ const BootcampSchema = new mongoose.Schema(
         toObject: { virtuals: true }
     }
 )
+
+BootcampSchema.pre('save', function (next) {
+    this.slug = slugify(this.name, { lower: true })
+    next()
+})
 
 module.exports = mongoose.model('Bootcamp', BootcampSchema)
