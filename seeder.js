9844ef8d74745459dc4d3fbb446e3ad332fbe765
Shaikh Mahboob <mahboob.shaikh@finclusionsolutions.com>
2023-10-19 22:58:43 +0530

    create seeder for courses

--- seeder.js
+++ seeder.js
@@ -7,16 +7,19 @@ dotenv.config({ path: './config/config.env' })
 
 // load models 
 const Bootcamp = require('./models/Bootcamp')
+const Course = require('./models/Course')
 
 // connect DB 
 mongoosoe.connect(process.env.MONGO_URI)
 
 // Read json file 
 const bootcamp = JSON.parse(fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8'))
+const courses = JSON.parse(fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8'))
 
 const importData = async () => {
     try {
         await Bootcamp.create(bootcamp)
+        await Course.create(courses)
         console.error(`data imported...`.green.inverse)
         process.exit(1)
     } catch (err) {
@@ -28,6 +31,7 @@ const importData = async () => {
 const deleteData = async () => {
     try {
         await Bootcamp.deleteMany()
+        await Course.deleteMany()
         console.log(`Data destroyed...`.red.inverse)
         process.exit(1)
     } catch (err) {
