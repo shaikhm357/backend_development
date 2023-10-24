1a14d3249b67eee1ba2eb8380f0d1b4b171db2dc
Shaikh Mahboob <mahboob.shaikh@finclusionsolutions.com>
2023-10-24 07:04:51 +0530

    created custom function for create token and send as cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -18,9 +18,7 @@ exports.registerUser = asyncHandler(async (req, res, next) => {
         role
     })
 
-    const token = user.getSignedJwtToken()
-
-    res.status(200).json({ success: true, data: user, token: token })
+    sentTokenResponse(user, 200, res)
 })
 
 // @dec        Login user
@@ -49,7 +47,25 @@ exports.login = asyncHandler(async (req, res, next) => {
         return next(new ErrorResponse(`Invalid credentials`, 401))
     }
 
+    sentTokenResponse(user, 200, res)
+})
+
+// Get token from model, create cookie and send response 
+
+const sentTokenResponse = (user, statusCode, res) => {
+    // create token 
     const token = user.getSignedJwtToken()
 
-    res.status(200).json({ success: true, token: token })
-})
+    const options = {
+        expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
+        httpOnly: true
+    }
+
+    if (process.env.NODE_ENV === 'production') {
+        options.secure = true
+    }
+
+    res.status(statusCode)
+        .cookie('token', token, options)
+        .json({ success: true, token })
+}
