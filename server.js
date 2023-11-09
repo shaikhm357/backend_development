3f435bfe52387b7991f8b1d51dfe12f33dd10b5d
Shaikh Mahboob <mahboob.shaikh@finclusionsolutions.com>
2023-11-09 11:39:26 +0530

    added security by adding extra headers using helmet and xss-clean for scripts in req body

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const colors = require('colors')
 const fileupload = require('express-fileupload')
 const cookieParser = require('cookie-parser')
 const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss = require('xss-clean');
 const errorHandler = require('./middleware/error')
 const connectDB = require('./config/db')
 
@@ -39,6 +41,12 @@ app.use(fileupload())
 
 // sanitize data
 app.use(mongoSanitize())
+
+// helmet
+app.use(helmet())
+
+// prevent xss attacks
+app.use(xss())
 
 // set static folder 
 app.use(express.static(path.join(__dirname, 'public')))
